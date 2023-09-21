a42006612e6c5cee86f9056eedbc0645b562384c
Jayson Roda <jayson.roda@awsys-i.com>
2023-09-21 16:53:36 +0800

    Add handle Prev and Next function, add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,59 @@
+import { useState } from "react";
+
+const messages = [
+    "Learn React ☀",
+    "Apply for Jobs 💼",
+    "Invest your income 🤑",
+];
+
 function App() {
-    return <></>;
+    const [step, setStep] = useState(1);
+    const [isOpen, setIsOpen] = useState(true);
+
+    function handlePrev() {
+        setStep((s) => s - 1);
+    }
+
+    function handleNext() {
+        setStep((s) => s + 1);
+    }
+    return (
+        <>
+            <button className="close" onClick={() => setIsOpen((io) => !io)}>
+                &times;
+            </button>{" "}
+            {isOpen && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 && "active"}>1</div>
+                        <div className={step >= 2 && "active"}>2</div>
+                        <div className={step >= 3 && "active"}>3</div>
+                    </div>
+
+                    <p className="message">
+                        Step {step}: {messages[step - 1]}
+                    </p>
+
+                    <div className="buttons">
+                        <button
+                            className={step > 1 && "enabled"}
+                            onClick={handlePrev}
+                            disabled={step === 1}
+                        >
+                            Previous
+                        </button>
+                        <button
+                            className={step < 3 && "enabled"}
+                            onClick={handleNext}
+                            disabled={step === 3}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default App;
